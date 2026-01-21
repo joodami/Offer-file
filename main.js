6d980441a33f58df0f0926dd816156f82dd5ff0c
joodami <joodami25@gmail.com>
2026-01-21 11:39:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,69 @@
 const GAS = 'https://script.google.com/macros/s/AKfycbycd0jLtPDxF17tZc4QGMGgLQktURjuJ_Q6SlFNA__wU-IRQKtfmVc6AtWqv-Lr5mkCpA/exec';
 let CODE = '';
 
-fetch(GAS+'?action=getData').then(r=>r.json()).then(d=>{
-  d.forEach(x=>{
+fetch(GAS + '?action=getData')
+.then(r => r.json())
+.then(data => {
+  data.forEach(x => {
     tb.innerHTML += `
-    <tr>
-      <td>${x[1]}</td>
-      <td>${x[2]}</td>
-      <td>${x[3]}</td>
-      <td>${x[3]=='พิจารณาเรียบร้อยแล้ว'
-        ? `<button onclick="openSign('${x[1]}')">รับแฟ้ม</button>`:'-'}</td>
-    </tr>`;
+      <tr>
+        <td>${x[1]}</td>
+        <td>${x[2]}</td>
+        <td>${x[3]}</td>
+        <td>
+          ${x[3] === 'พิจารณาเรียบร้อยแล้ว'
+            ? `<button onclick="openSign('${x[1]}')">รับแฟ้มคืน</button>`
+            : '-'}
+        </td>
+      </tr>`;
   });
 });
 
-function add(){
- fetch(GAS,{
-  method:'POST',
-  body:JSON.stringify({
-    action:'add',
-    date:date.value,
-    code:code.value,
-    sender:sender.value
-  })
- }).then(()=>location.reload());
+function add() {
+  fetch(GAS, {
+    method: 'POST',
+    body: JSON.stringify({
+      action: 'add',
+      date: date.value,
+      code: code.value,
+      sender: sender.value
+    })
+  }).then(() => location.reload());
 }
 
-// Canvas Touch
+/* Canvas รองรับมือถือ */
 const c = document.getElementById('c');
 const ctx = c.getContext('2d');
-let draw=false;
+let draw = false;
 
-['mousedown','touchstart'].forEach(e=>c.addEventListener(e,()=>draw=true));
-['mouseup','touchend'].forEach(e=>c.addEventListener(e,()=>draw=false));
+c.addEventListener('pointerdown', () => draw = true);
+c.addEventListener('pointerup', () => draw = false);
+c.addEventListener('pointermove', e => {
+  if (!draw) return;
+  ctx.lineWidth = 2;
+  ctx.lineCap = 'round';
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+});
 
-c.addEventListener('mousemove',drawLine);
-c.addEventListener('touchmove',drawLine);
-
-function drawLine(e){
- if(!draw) return;
- const r=c.getBoundingClientRect();
- const x=(e.touches?e.touches[0].clientX:e.clientX)-r.left;
- const y=(e.touches?e.touches[0].clientY:e.clientY)-r.top;
- ctx.lineWidth=2; ctx.lineCap='round';
- ctx.lineTo(x,y); ctx.stroke();
+function clearC() {
+  ctx.clearRect(0, 0, c.width, c.height);
 }
 
-function clearC(){ ctx.clearRect(0,0,c.width,c.height); }
+function openSign(code) {
+  CODE = code;
+  sign.style.display = 'block';
+}
 
-function openSign(code){ CODE=code; sign.style.display='block'; }
-
-function save(){
- fetch(GAS,{
-  method:'POST',
-  body:JSON.stringify({
-    action:'receive',
-    code:CODE,
-    receiver:receiver.value,
-    date:new Date().toLocaleDateString(),
-    signature:c.toDataURL()
-  })
- }).then(()=>location.reload());
+function save() {
+  fetch(GAS, {
+    method: 'POST',
+    body: JSON.stringify({
+      action: 'receive',
+      code: CODE,
+      receiver: receiver.value,
+      date: new Date().toLocaleDateString(),
+      signature: c.toDataURL()
+    })
+  }).then(() => location.reload());
 }
