4fa1eb73f7481195e97e16bf45d55c199d44f70e
joodami <joodami25@gmail.com>
2026-01-29 10:47:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -29,15 +29,17 @@ function showToast(msg, success = true) {
 }
 
 function showMobileLoading() {
-  if (!isMobile()) return;
+if (!isMobile()) return;
 
-  cardView.innerHTML = `
-    <div class="card shadow-sm p-4 text-center text-muted mt-3">
+cardView.innerHTML = `
+  <div class="card shadow-sm mt-3" style="min-height: 180px;">
+    <div class="d-flex flex-column justify-content-center align-items-center h-100 text-muted">
       <div class="spinner-border text-primary mb-3"></div>
       <div>กำลังโหลดข้อมูล...</div>
     </div>
-  `;
-}
+  </div>
+`;
+
 
 
 /* =====================
