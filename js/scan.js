d4fa467eafbc32bfd82f244b219f25f69d635e93
joodami <joodami25@gmail.com>
2026-02-03 14:31:44 +0700

    Update scan.js

--- js/scan.js
+++ js/scan.js
@@ -1,1 +1,42 @@
+const fid = getParam('fid');
+
+if (!fid) {
+  alert('QR ไม่ถูกต้อง');
+  location.href = 'index.html';
+}
+
+checkStatus();
+
+async function checkStatus() {
+  const r = await post('getFileStatus', { fileId: fid });
+  if (!r || !r.success) {
+    alert('ไม่พบข้อมูลแฟ้ม');
+    return;
+  }
+
+  redirectByStatus(r.status);
+}
+
+function redirectByStatus(status) {
+
+  if (status === 'NEW') {
+    location.replace('submit.html?fid=' + fid);
+    return;
+  }
+
+  if (status === 'SUBMITTED') {
+    location.replace('status_submit.html?fid=' + fid);
+    return;
+  }
+
+  if (status === 'APPROVED') {
+    location.replace('status_approved.html?fid=' + fid);
+    return;
+  }
+
+  if (status === 'RECEIVED') {
+    location.replace('status_received.html?fid=' + fid);
+    return;
+  }
+}
 
